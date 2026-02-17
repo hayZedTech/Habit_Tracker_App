ea482115421e87fed64c95b7ff3858ad7d4b4faa
Azeez Ololade <ololadeazeez.m@gmail.com>
2026-02-17 23:23:16 +0100

    Cleaned up tracking and applied gitignore

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,7 @@ function RouteGuard({ children }: { children: React.ReactNode }) {
   if (isLoading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color="#0000ff" />   
       </View>
     );
   }
